6f550fc5fa97895a15ba3c8eee1ecd0c32cb70eb
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-06-21 22:13:18 +0300

    task-10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import { countryСardTemplate, countryListTemplate } from './markupTemplate';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
@@ -50,3 +49,23 @@ function onInputCountry() {
       return error;
     });
 }
+
+function countryСardTemplate({flags,name,capital,population,languages,}) {
+  return `
+      <img class="country-info__flags" src="${flags.svg}" alt="${name.official}" width="100" />
+      <h2 class="country-info__name">${name.official}</h2>
+      <p class="country-info__capital">Capital: ${capital}</p>
+      <p class="country-info__population">Population: ${population}</p>
+      <p class="country-info__languages">Languages: ${Object.values(languages)}</p>
+  `;}
+
+function countryListTemplate(response) {
+  return `
+  <li class="country-list__item">
+    <img class="country-list__flags" 
+      src="${response.flags.svg}" 
+      alt="${response.name.official}" width="30"
+    />
+    <h5 class="country-list__name">${response.name.official}</h5>
+  </li>
+  `;}
